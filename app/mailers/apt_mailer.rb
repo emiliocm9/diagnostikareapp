4d22089811189eb5afa373aba364e7501850b5ae
emiliocm9 <emilio.contreras97@gmail.com>
2020-12-16 17:56:54 -0600

    mail: new appointment method

--- app/mailers/apt_mailer.rb
+++ app/mailers/apt_mailer.rb
@@ -1,2 +1,6 @@
 class AptMailer < ApplicationMailer
+  def new_appointment_email(user)
+    @appointment = params[:appointment]
+    mail(to: user.email, subject: "You have scheduled a new appointment!")
+  end
 end
